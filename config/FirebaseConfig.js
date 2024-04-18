ef931cf57cad9f30fb2bb61891f93693c26dba61
Ashutosh <161562995+Ashutosh0x@users.noreply.github.com>
2024-04-18 19:33:26 +0530

    FirebaseConfig.js

--- config/FirebaseConfig.js
+++ config/FirebaseConfig.js
@@ -8,14 +8,14 @@ import { getAnalytics } from "firebase/analytics";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "apps-e02e4.firebaseapp.com",
-  databaseURL: "https://apps-e02e4-default-rtdb.firebaseio.com",
-  projectId: "apps-e02e4",
-  storageBucket: "apps-e02e4.appspot.com",
-  messagingSenderId: "931728242416",
-  appId: "1:931728242416:web:413bcf5fe9d40eef293435",
-  measurementId: "G-JD421BHN8R"
+  authDomain: "meeting-scheduler-34971.firebaseapp.com",
+  projectId: "meeting-scheduler-34971",
+  storageBucket: "meeting-scheduler-34971.appspot.com",
+  messagingSenderId: "184885689199",
+  appId: "1:184885689199:web:4dd4b7224be56d3c09869a",
+  measurementId: "G-82J67KQ37K"
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+
